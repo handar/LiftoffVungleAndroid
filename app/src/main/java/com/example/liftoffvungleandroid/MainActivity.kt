0d291927f8c0c77c3f6e6c7eeffa4b9b9dff2f71
handar <hadiaandar@gmail.com>
2025-02-20 20:23:51 -0800

    Integrated vungle sdk

---
+++ app/src/main/java/com/example/liftoffvungleandroid/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.liftoffvungleandroid
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.liftoffvungleandroid.ui.theme.LiftoffVungleAndroidTheme
+import com.vungle.ads.InitializationListener
+import com.vungle.ads.VungleAds
+import com.vungle.ads.VungleError
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Initialize Vungle SDK
+        VungleAds.init(this, "643d1db1143d3bfd6bcf6510", object : InitializationListener {
+            override fun onSuccess() {
+                Log.d(TAG, "Vungle SDK init onSuccess()")
+            }
+            override fun onError(vungleError: VungleError) {
+                Log.d(TAG, "onError(): ${vungleError.localizedMessage}")
+            }
+        })
+
+        enableEdgeToEdge()
+        setContent {
+            LiftoffVungleAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LiftoffVungleAndroidTheme {
+        Greeting("Android")
+    }
+}
